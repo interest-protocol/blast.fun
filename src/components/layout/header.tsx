79eaf4c3764b81c4d1387685979fdd29e8a5ab13
matical <defyglide@gmail.com>
2025-07-16 16:16:08 -0700

    fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,8 +5,10 @@ import { ThemeSwitcher } from "../shared/theme-switcher";
 import { UserDropdown } from "../user/user-dropdown";
 import Balance from "../balance";
 import { Skull } from "lucide-react";
+import { useApp } from "@/context/app.context";
 
 export default function Header() {
+    const { isConnected } = useApp();
 
     return (
         <header className="flex h-14 sticky top-0 border-b-2 bg-background/80 backdrop-blur-md z-50">
@@ -24,8 +26,13 @@ export default function Header() {
                 </div>
 
                 <div className="flex items-center justify-end gap-3">
-                    <Balance />
-                    <div className="h-6 w-[1px] bg-foreground/20" />
+                    {isConnected && (
+                        <>
+                            <Balance />
+                            <div className="h-6 w-[1px] bg-foreground/20" />
+                        </>
+                    )}
+
                     <UserDropdown />
                     <div className="h-6 w-[1px] bg-foreground/20" />
                     <ThemeSwitcher />
