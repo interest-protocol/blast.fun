8b12d412336ea0f57e96953f454518875162aafe
matical <defyglide@gmail.com>
2025-07-15 17:02:19 -0700

    init prisma

--- src/env.ts
+++ src/env.ts
@@ -3,14 +3,20 @@ import { z } from 'zod'
 
 export const env = createEnv({
     server: {
+        DATABASE_URL: z.string().min(1),
+
         TWITTER_CLIENT_SECRET: z.string().min(1),
         TWITTER_CLIENT_ID: z.string().min(1),
         TWITTER_REDIRECT_URI: z.url(),
     },
     client: {
-        // 
+        NEXT_PUBLIC_FEE_ADDRESS: z.string().min(1),
     },
     runtimeEnv: {
+        NEXT_PUBLIC_FEE_ADDRESS: process.env.NEXT_PUBLIC_FEE_ADDRESS,
+
+        DATABASE_URL: process.env.DATABASE_URL,
+
         TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
         TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
         TWITTER_REDIRECT_URI: process.env.TWITTER_REDIRECT_URI
