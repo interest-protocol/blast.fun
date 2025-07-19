641629b228a3213c16c80feb17432e16b56fdaa3
matical <defyglide@gmail.com>
2025-07-19 12:29:33 -0700

    refac: make user balance data red

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -11,9 +11,9 @@ const Balance = () => {
 	return (
 		<Button
 			variant="outline"
-			className="rounded-xl px-2 !bg-orange-400/20 !border-orange-400 ease-in-out duration-300 transition-all"
+			className="rounded-xl px-2 !border-destructive/50 !bg-destructive/10 text-destructive [&>svg]:text-current hover:text-destructive/80"
 		>
-			<span className="dark:text-orange-200 text-orange-400 group-hover:text-primary transition-colors duration-300 font-semibold text-sm">
+			<span className="font-semibold text-sm">
 				{balance} SUI
 			</span>
 		</Button>
