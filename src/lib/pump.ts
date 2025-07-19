7c0e8925349bb5daa107904912ff02d5f814d43e
matical <defyglide@gmail.com>
2025-07-19 10:44:21 -0700

    chore: bump memez sdk to 7.2.0

--- src/lib/pump.ts
+++ src/lib/pump.ts
@@ -2,7 +2,6 @@ import { MemezPumpSDK } from "@interest-protocol/memez-fun-sdk"
 import { getFullnodeUrl } from "@mysten/sui/client"
 import { env } from "@/env"
 
-// @dev: we just hardcode this for now, but we can add a env later :(
 export const pumpSdk = new MemezPumpSDK({
 	network: env.NEXT_PUBLIC_DEFAULT_NETWORK,
 	fullNodeUrl: getFullnodeUrl(env.NEXT_PUBLIC_DEFAULT_NETWORK),
