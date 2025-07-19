bf939d6f1c6b2b2ac20239ebd7dee3ef2493a4a0
matical <defyglide@gmail.com>
2025-07-18 19:01:17 -0700

    refac: 40k -> 20k target mcap

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const DEFAULT_TOKEN_DECIMALS = 9;
 
 export const TOTAL_POOL_SUPPLY = 1_000_000_000n * (10n ** 9n); // 1 billion * 10^9 (for 9 decimals)
 export const VIRTUAL_LIQUIDITY = 500 * (10 ** 9); // 500 * 10^9 (for 9 decimals)
-export const TARGET_QUOTE_LIQUIDITY = 10_000 * (10 ** 9); // 10,000 * 10^9 (for 9 decimals)
+export const TARGET_QUOTE_LIQUIDITY = 5_000 * (10 ** 9); // 5,000 * 10^9 (for 9 decimals)
 
 export const COIN_CONVENTION_BLACKLIST = [
     'SUI',
