b237c2932b25ef6f28c6ef3df2c09f439fe2f085
matical <defyglide@gmail.com>
2025-07-19 12:35:53 -0700

    refac(style): remove max width classes from root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({
 			<Header />
 
 			<main className="flex-1 overflow-y-auto pb-14 md:pb-0">
-				<div className="mx-auto max-w-7xl px-4 py-6">{children}</div>
+				<div className="p-6">{children}</div>
 			</main>
 
 			<MobileNav />
