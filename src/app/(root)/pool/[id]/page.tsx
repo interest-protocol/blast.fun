a4e113a370977fada7761702cc0f31d577800582
matical <defyglide@gmail.com>
2025-07-20 20:05:41 -0700

    fix: no need for async anymore

--- src/app/(root)/pool/[id]/page.tsx
+++ src/app/(root)/pool/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { use } from "react"
 import Pool from "./_components/pool"
 
-export default async function PoolPage({ params }: { params: Promise<{ id: string }> }) {
+export default function PoolPage({ params }: { params: Promise<{ id: string }> }) {
 	const { id } = use(params)
 
 	return <Pool poolId={id} />
