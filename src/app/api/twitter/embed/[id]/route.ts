c88b5f86fd49b9a1782884f8fe35285cb8ab9f94
matical <defyglide@gmail.com>
2025-07-22 17:14:10 -0700

    feat: add favicon + logos

--- src/app/api/twitter/embed/[id]/route.ts
+++ src/app/api/twitter/embed/[id]/route.ts
@@ -28,7 +28,7 @@ export async function GET(
     <meta name="twitter:card" content="player" />
     <meta name="twitter:creator" content="@xpumpfun">
     <meta name="twitter:site" content="@xpumpfun" />
-    <meta name="twitter:image" content="${appUrl}/embed/xpump-twitter-logo.png" />
+    <meta name="twitter:image" content="${appUrl}/logo/xpump-logo.png" />
     <meta name="twitter:title" content="Trade memecoins on xPump directly within X." />
     <meta name="twitter:description" content="Buy & sell memecoins on xPump directly within X. Just click me and you'll see!" />
 
@@ -41,7 +41,7 @@ export async function GET(
     <meta property="og:type" content="video.movie" />
     <meta property="og:title" content="Trade memecoins on xPump directly within X." />
     <meta property="og:description" content="Connect wallet and trade tokens directly" />
-    <meta property="og:image" content="${appUrl}/embed/xpump-twitter-logo.png" />
+    <meta property="og:image" content="${appUrl}/logo/xpump-logo.png" />
     <meta property="og:url" content="${appUrl}/x-card/${id}" />
     
     <title>xPump Trading</title>
