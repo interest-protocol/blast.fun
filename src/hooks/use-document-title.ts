56c8b7ca82b303180f53b2536b80030ff7b1922f
matical <defyglide@gmail.com>
2025-08-05 17:12:36 -0700

    feat: add clientside document title setter

---
+++ src/hooks/use-document-title.ts
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+import { siteConfig } from "@/constants"
+
+interface UseDocumentTitleOptions {
+	symbol?: string
+	marketCap?: number | string
+	formatMarketCap?: (value: number | string) => string
+}
+
+export function useDocumentTitle(options: UseDocumentTitleOptions = {}) {
+	const { symbol, marketCap, formatMarketCap } = options
+
+	useEffect(() => {
+		if (symbol && marketCap) {
+			const formattedMarketCap = formatMarketCap
+				? formatMarketCap(marketCap)
+				: typeof marketCap === 'number'
+					? `$${marketCap.toLocaleString()}`
+					: marketCap
+
+			document.title = `${symbol.toUpperCase()} ${formattedMarketCap} | ${siteConfig.name}`
+		} else if (symbol) {
+			document.title = `${symbol.toUpperCase()} | ${siteConfig.name}`
+		} else {
+			document.title = siteConfig.name
+		}
+
+		return () => {
+			document.title = siteConfig.name
+		}
+	}, [symbol, marketCap, formatMarketCap])
+}
