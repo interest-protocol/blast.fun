a05632008f8805521818ac3ceb12a896f4fa1d54
matical <defyglide@gmail.com>
2025-08-08 10:07:19 -0700

    feat: add ipx label

--- src/constants/wallet-labels.ts
+++ src/constants/wallet-labels.ts
@@ -4,6 +4,7 @@
  */
 export const WALLET_LABELS: Record<string, string> = {
 	"0x881d835c410f33a1decd8067ce04f6c2ec63eaca196235386b44d315c2152797": "BLAST.FUN TREASURY",
+	"0x5b2aec3521419fe055b6753b15cbad845ec1dca852b75ad0b13b569f2329f82d": "IPX FEES",
 	"0xdc19b2928f31b6df46478e4ad9a309aaff6e958a3b568d4bb76264f767bdfbfc": "GIVEREP TEAM",
 }
 
