a5613a4300fcd4526a38f4316137804e2ab79092
matical <defyglide@gmail.com>
2025-08-11 13:07:47 -0700

    feat: init migrator sdk

--- src/lib/pump.ts
+++ src/lib/pump.ts
@@ -1,9 +1,16 @@
-import { MemezPumpSDK } from "@interest-protocol/memez-fun-sdk"
+import { MemezPumpSDK, XPumpMigratorSDK } from "@interest-protocol/memez-fun-sdk"
 import { getFullnodeUrl } from "@mysten/sui/client"
 import { env } from "@/env"
 import { Network } from "@/types/network"
 
+const fullNodeUrl = getFullnodeUrl(env.NEXT_PUBLIC_DEFAULT_NETWORK)
+
 export const pumpSdk = new MemezPumpSDK({
 	network: env.NEXT_PUBLIC_DEFAULT_NETWORK as Network,
-	fullNodeUrl: getFullnodeUrl(env.NEXT_PUBLIC_DEFAULT_NETWORK),
+	fullNodeUrl
+})
+
+export const migratorSdk = new XPumpMigratorSDK({
+	network: env.NEXT_PUBLIC_DEFAULT_NETWORK as Network,
+	fullNodeUrl
 })
