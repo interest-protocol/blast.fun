c1bd0c246cac7a42d0a6c3a2475677e41844f958
EasonC13 <eason.tw.chen@gmail.com>
2025-08-16 22:53:29 -0400

    show latest profile image

--- src/components/user/user-avatar.tsx
+++ src/components/user/user-avatar.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { TwitterUser } from "@/context/twitter.context"
+import { getFxtwitterProfileImage } from "@/lib/twitter"
 import { cn } from "@/utils"
+import { useEffect, useState } from "react"
 
 interface UserAvatarProps {
 	user: TwitterUser
@@ -9,5 +11,20 @@ interface UserAvatarProps {
 }
 
 export function TwitterUserAvatar({ user, className }: UserAvatarProps) {
-	return <img src={user.profile_image_url || ""} className={cn("rounded-md", className)} />
+	const [profileImageUrl, setProfileImageUrl] = useState<string | null>(user.profile_image_url)
+
+	useEffect(() => {
+		const fetchUpdatedProfileImage = async () => {
+			if (user.username) {
+				const updatedImage = await getFxtwitterProfileImage(user.username)
+				if (updatedImage) {
+					setProfileImageUrl(updatedImage)
+				}
+			}
+		}
+		fetchUpdatedProfileImage()
+	}, [user.username])
+	console.log({profileImageUrl});
+
+	return <img src={profileImageUrl || user.profile_image_url || ""} className={cn("rounded-md", className)} />
 }
