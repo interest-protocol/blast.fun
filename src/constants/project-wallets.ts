e45a3702b18bc980baf49c403490da0801c13217
notdeath <103466637+0xd34th@users.noreply.github.com>
2025-09-16 16:49:22 -0600

    Update project-wallets.ts

--- src/constants/project-wallets.ts
+++ src/constants/project-wallets.ts
@@ -1,6 +1,7 @@
 // @dev: List of ecosystem project wallets to be displayed in the Top Projects tab
 export const PROJECT_WALLETS: Record<string, string> = {
 	"0x22441936a0d6fd21d07d596813dfa29fbc54d44b94eb87916fbcb51d639fde96": "Blast",
+	"0xbd23d48e04fac22ae7dc79bdd13a3c25b812a8eaa15a245a3c1d37cc06ae94cc": "Mission69b",
 	"0xa07c5cc3749ea6e0cb9e713d55020f700be29719f4a1f2fb1bc94fd88058590f": "Studio Mirai",
 	"0x31b1856481ec441bc0898f0120ef44bb8be5ab1aed2866de3f4f75d79a73e91e": "Puggies",
 	// @dev: Add more project wallets here as needed
