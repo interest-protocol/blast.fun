553c0c4080432153ed041fab5709f23bf8f60a81
Cyber <shaibuafeez@gmail.com>
2025-10-07 21:47:58 +0100

    refactor(graphql): remove GET_POOL_QUICK_BUY query

--- src/graphql/pools.ts
+++ src/graphql/pools.ts
@@ -101,13 +101,3 @@ export const GET_POOLS_BATCH = gql`
 		}
 	}
 `
-
-export const GET_POOL_QUICK_BUY = gql`
-	query GetPoolQuickBuy($type: String!) {
-		coinPool(type: $type) {
-			poolId
-			metadata
-			migrated
-		}
-	}
-`
