ba8ef06f35095c18202e7e1c8b445698e1a7f01e
Cyber <shaibuafeez@gmail.com>
2025-10-08 13:25:07 +0100

    revert: restore ReactScan component and blake2b import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next"
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { SessionProvider } from "next-auth/react"
 import { Toaster } from "react-hot-toast"
+import { ReactScan } from "@/components/utils/react-scan"
 import { TailwindIndicator } from "@/components/utils/tailwind-indicator"
 import { TwitterAuthProvider } from "@/context/twitter.context"
 import { geistMono, geistSans } from "@/fonts"
@@ -57,6 +58,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<ReactScan />
 			<body className={cn(geistSans.variable, geistMono.variable, "antialiased")}>
 				<ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
 					<TooltipProvider>
