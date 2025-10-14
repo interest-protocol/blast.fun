78a77876020bab86cf7fb5edaeff559de40f9082
matical <defyglide@gmail.com>
2025-10-14 16:05:07 -0700

    refac: move stylesheets to /styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import "@/app/globals.css"
-import "@/app/animations.css"
+import "@/styles/globals.css"
+import "@/styles/animations.css"
 
 import type { Metadata } from "next"
 import { GoogleAnalytics } from '@next/third-parties/google'
