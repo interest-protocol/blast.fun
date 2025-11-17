aff34ec08ddedcc50ca17928ddf26893fd88d6e0
Marco <marcomorete73@hotmail.com>
2025-11-17 22:03:40 +0100

    :fire: chore: remove name limit

--- src/app/(root)/launch/_components/create-token-form.tsx
+++ src/app/(root)/launch/_components/create-token-form.tsx
@@ -18,7 +18,7 @@ import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible"
 import { useBalance } from "@/hooks/sui/use-balance"
 
 const tokenSchema = z.object({
-	name: z.string().min(3, "Minimum 3 characters").max(20, "Maximum 20 characters"),
+	name: z.string().min(3, "Minimum 3 characters"),
 	symbol: z
 		.string()
 		.min(2, "Minimum 2 characters")
