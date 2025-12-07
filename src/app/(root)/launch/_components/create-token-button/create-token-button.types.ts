66480e6d597a18645bfe492786c1daf4ed640e8d
Marco <marcomorete73@hotmail.com>
2025-12-07 21:18:37 +0100

    :green_heart: build: fix

--- src/app/(root)/launch/_components/create-token-button/create-token-button.types.ts
+++ src/app/(root)/launch/_components/create-token-button/create-token-button.types.ts
@@ -1,6 +1,6 @@
-import { UseFormReturn } from "react-hook-form"
-import { TokenFormValues } from "../create-token-form"
+import { UseFormReturn } from "react-hook-form";
+import { TokenFormValues } from "../create-token-form/create-token-form.types";
 
 export interface CreateTokenButtonProps {
-	form: UseFormReturn<TokenFormValues>
+    form: UseFormReturn<TokenFormValues>;
 }
