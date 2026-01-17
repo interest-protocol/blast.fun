1bb922a85243456f59745360537a4312d1f1e777
LuyandraBranco <luyandrabranco@gmail.com>
2026-01-17 12:30:25 +0100

    🚨 fix: import in use airdrop

--- src/views/tools/_components/airdrop/_hooks/use-airdrop.ts
+++ src/views/tools/_components/airdrop/_hooks/use-airdrop.ts
@@ -6,9 +6,8 @@ import { SUI_DECIMALS, SUI_TYPE_ARG } from "@mysten/sui/utils"
 import { suiClient } from "@/lib/sui-client"
 import { useTransaction } from "@/hooks/sui/use-transaction"
 import toast from "react-hot-toast"
-import { AirdropRecipient } from "../_components/airdrop-tools/airdrop-tools.types"
 import { BATCH_SIZE, GAS_PER_RECIPIENT, SERVICE_FEE_PER_RECIPIENT, DELEGATOR_KEY_STORAGE } from "../airdrop.consts"
-
+import { AirdropRecipient } from "../airdrop-tools/airdrop-tools.types"
 interface UseAirdropParams {
 	address: string | null | undefined
 	selectedCoin: string
